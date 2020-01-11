f96f3d22bc3da544c8f46a9edd8c8417406c3248
Rogin Farrer <rfarrer@wayfair.com>
2020-01-11 01:31:15 -0500

    add some stuff

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
 module.exports = {
   extends: [require.resolve('eslint-config-kentcdodds')],
-  rules: {
-    'react/no-did-mount-set-state': 'off',
-    'react/default-props-match-prop-types': [
-      'error',
-      {allowRequiredDefaults: true},
-    ],
-  },
 };
